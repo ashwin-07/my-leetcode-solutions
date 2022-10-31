71f307fa3a7760489d6f0cfe8d9266545ba6a4a1
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2022-10-31 18:52:01 +0530

    Time: 23 ms (61.46%), Space: 39.9 MB (82.77%)

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,24 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+        int lo = 0, hi = n;
+        
+        while (lo < hi) {
+            
+            int mid = lo + (hi - lo)/2;
+            if (isBadVersion(mid)) {
+                hi = mid;
+            }
+            else {
+                lo = mid + 1;
+            }
+            
+        }
+        
+        return lo;
+        
+    }
+}
