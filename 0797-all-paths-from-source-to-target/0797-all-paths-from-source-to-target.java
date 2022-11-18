a2b0ab31dafc2293194881d850d56cc6978b123c
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2022-11-18 20:24:34 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (92.08%)

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    List<List<Integer>> paths = new ArrayList<>();
+    
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        
+        
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        
+        findPaths(path, graph, 0);
+        
+        
+        
+        return paths;
+        
+    }
+    
+    
+    public void findPaths(List<Integer> path, int[][] graph, int node) {
+        
+        if (node == graph.length - 1) {
+            paths.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for (int i: graph[node]) {
+            path.add(i);
+            findPaths(path, graph, i);
+            path.remove(path.size() - 1);
+        }
+        
+        
+    }
+}
