34d6e1686e40c30ae041fb7be86c48aa638e55af
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2022-12-12 21:52:51 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (46.49%)

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    
+    public int climbStairs(int n) {
+        
+        if (n <= 2){
+            return n;
+        }
+        
+        int a = 1;
+        int b = 2;
+        
+        for (int i = 3; i <= n; i++) {          
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        
+        return b;
+    }
+}
