3f9feea786984e09c2f4ad2882e0c4d734452533
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-01-04 21:19:33 +0530

    Time: 82 ms (44.00%), Space: 105.5 MB (30.26%)

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -12,25 +12,10 @@ class Solution {
         for (int diffLevel:difficultyLvlCounts.keySet()) {
             
             int count = difficultyLvlCounts.get(diffLevel);
-            
-            if(count < 2) {
+            if (count == 1) {
                 return -1;
             }
-            
-            int countMod3 = count % 3;
-            int countBy3 = count / 3;
-            
-            if (countMod3 == 0) {
-                sum+=countBy3;
-            }
-            else if (countMod3 == 1) {
-                int noOf3s = countBy3 - 1;
-                int noOf2s = (count - (3 * noOf3s))/2;
-                sum+=noOf3s+noOf2s;
-            }
-            else {
-                sum+=countBy3+1;
-            }
+            sum+=((count+2)/3);
         }
         return sum;
     }
