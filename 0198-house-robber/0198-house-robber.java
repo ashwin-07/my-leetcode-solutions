624fc45f21ebc0c9411f0b8616f6eb17ec9b57cf
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-01-04 21:41:05 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (73.94%)

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,32 @@
 class Solution {
     public int rob(int[] nums) {
         
-        int[] dp = new int[nums.length+1];
-        dp[0] = 0;
-        dp[1] = nums[0];
+        //T: O(n)   S: O(n)
+//         int[] dp = new int[nums.length+1];
+//         dp[0] = 0;
+//         dp[1] = nums[0];
         
-        for (int i = 1; i < nums.length; i++) {
-            dp[i+1] = Math.max(dp[i-1]+nums[i], dp[i]);
+//         for (int i = 1; i < nums.length; i++) {
+//             dp[i+1] = Math.max(dp[i-1]+nums[i], dp[i]);
+//         }
+        
+//         return dp[nums.length];
+        
+        //T: O(n)   S: O(1)
+        
+        int prev1 = 0;
+        int prev2 = 0;
+        
+        for (int num:nums) {
+            
+            int temp = prev1;
+            prev1 = Math.max(prev2+num, prev1);
+            prev2 = temp;
+            
         }
         
-        return dp[nums.length];
+        return prev1;
+          
         
     }
 }
