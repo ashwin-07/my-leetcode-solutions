280003a2f716b930a183a326e937f2664660973f
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-01-04 22:16:08 +0530

    Time: 1 ms (95.90%), Space: 40.9 MB (71.30%)

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numDecodings(String s) {
+        
+        int dp[] = new int[s.length() + 1];
+        dp[0] = 1;
+        if (s.charAt(0) > '0') {
+            dp[1] = 1;
+        }
+        
+        for (int i = 2; i <= s.length(); i++) {
+            
+            int oneDigit = Integer.parseInt(s.substring(i-1, i));
+            int twoDigit = Integer.parseInt(s.substring(i-2,i));
+            
+            if (oneDigit > 0 && oneDigit <= 9) {
+                dp[i]=dp[i-1];
+            }
+            
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i]+=dp[i-2];
+            }
+            
+        }
+        
+        return dp[s.length()];
+        
+    }
+}
