2049235ccad76860e6e2201b257d278e68942060
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-01-09 23:55:38 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (66.12%)

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+    
+    public boolean isValidBST(TreeNode root, TreeNode max, TreeNode min) {
+        
+        if (root == null) {
+            return true;
+        }
+        
+        if (max != null && root.val >= max.val) {
+            return false;
+        }
+        if (min != null && root.val <= min.val) {
+            return false;
+        }
+        
+        return isValidBST(root.left, root, min) && isValidBST(root.right, max, root);
+        
+    }
+    
+
+}
