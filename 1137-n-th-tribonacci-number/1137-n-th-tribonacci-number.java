4634b5f1bb2bae9ec4f696c05f1c130fbbd598b3
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-01-30 10:10:44 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (61.90%)

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -2,27 +2,21 @@ class Solution {
     
     public int tribonacci(int n) {
         
-        if (n == 0) {
-            return 0;
-        } else if (n <= 2) {
-            return 1;
-        }
+        if (n < 2) {
+            return n;
+        } 
         
         int prev1 = 1;
         int prev2 = 1;
         int prev3 = 0;
         
         int itr = n - 2;
-        int curr = 0;
+        int curr = 1;
         while (itr-- > 0) {
-            
             curr = prev1+prev2+prev3;
-            int temp = prev1;
-            int temp2 = prev2;
-            prev1 = curr;
-            prev2 = temp;
-            prev3 = temp2;
-            
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = curr; 
         }
         
         return curr;
