3082afbf4e7c45f80d87b22462fa37d15ee97868
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-03-03 22:25:38 +0530

    Time: 1 ms (41.61%), Space: 40 MB (86.86%)

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        //brute force solution
+        
+        if (needle.length() == 0) {
+            return 0;
+        }
+        
+        if (haystack.length() == 0) {
+            return -1;
+        }
+        
+        
+        for (int i = 0; i < haystack.length(); i++) {
+            
+            if (i + needle.length() > haystack.length()) {
+                break;
+            }
+            
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i+j) != needle.charAt(j)) {
+                    break;
+                }
+                if (j == needle.length()-1) {
+                    return i;
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
