3a4837055fd3a50539bae6a1e76106fb685ffb7f
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-03-04 16:21:13 +0530

    Time: 7 ms (84.52%), Space: 60.9 MB (19.19%)

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
@@ -1,7 +1,7 @@
 class Solution {
     public long countSubarrays(int[] nums, int minK, int maxK) {
         
-        int currBoundStartIdx = -1;
+        int currBoundStartIdx = 0;
         int minNumIdx = -1;
         int maxNumIdx = -1;
         
@@ -10,7 +10,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             
             if (nums[i] > maxK || nums[i] < minK) {
-                currBoundStartIdx = i;
+                currBoundStartIdx = i+1;
             }
             
             if (nums[i] == minK) {
@@ -20,7 +20,7 @@ class Solution {
                 maxNumIdx = i;
             }
             
-            numOfSubArrs+=Math.max(0, Math.min(maxNumIdx, minNumIdx) - currBoundStartIdx);
+            numOfSubArrs+=Math.max(0, Math.min(maxNumIdx, minNumIdx) - currBoundStartIdx+1);
             
         }
         
