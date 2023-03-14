a87f361257865508db650ccecb4334733644d81c
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-03-14 23:34:27 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (70.36%)

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        
+        return findSum(root, 0);
+    }
+    
+    public int findSum(TreeNode root, int sum) {
+        
+        if (root == null) {
+            return 0;
+        }
+        
+        if (root.left == null && root.right == null){
+            return sum*10 + root.val;
+        }
+        return findSum(root.left, (sum*10)+root.val) + findSum(root.right, (sum*10)+root.val);
+    }
+}
