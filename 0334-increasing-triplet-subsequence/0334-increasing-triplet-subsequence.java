9c2cf6191450ec900aee600061fe7b55d40000ce
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-05-19 23:33:29 +0530

    Time: 3 ms (44.46%), Space: 132.1 MB (7.49%)

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
         
-        int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE, c = Integer.MAX_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE, min3 = Integer.MAX_VALUE;
         
         if (nums.length < 3){
             return false;
-        }
-        
+        }   
         
         for (int i = 0; i < nums.length; i++) {
             
-            if (nums[i] <= a) {
-                a = nums[i];
+            if (nums[i] <= min1) {
+                min1 = nums[i];
             }
-            else if (nums[i] <= b && nums[i] > a) {
-                b = nums[i];
+            else if (nums[i] <= min2 && nums[i] > min1) {
+                min2 = nums[i];
             }
-            else if (nums[i] <= c && nums[i] > b) {
-                c = nums[i];
+            else if (nums[i] <= min3 && nums[i] > min2) {
+                min3 = nums[i];
                 return true;
             }
         }
