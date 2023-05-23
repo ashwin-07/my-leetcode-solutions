29f2a51b9c3e6e3b7dfb4959ecbbdcb28e5fa05e
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-05-23 23:59:05 +0530

    Time: 17 ms (53.19%), Space: 49.5 MB (6.00%)

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,26 @@
+class KthLargest {
+
+    int k;
+    Queue<Integer> minHeap = new PriorityQueue<>();
+    
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        for (int num : nums) {
+            add(num);
+        }
+    }
+    
+    public int add(int val) {
+        minHeap.add(val);
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+        return minHeap.peek();        
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
