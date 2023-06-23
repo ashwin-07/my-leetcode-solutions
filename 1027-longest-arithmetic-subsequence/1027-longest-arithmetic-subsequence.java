63c4b378ef2bdd3f5f19839f9e704ac960be9f61
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-06-23 22:25:17 +0530

    Time: 649 ms (48.10%), Space: 69.4 MB (37.64%)

---
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestArithSeqLength(int[] nums) {
+        
+        int maxSeqLen = 0;
+        List<Map<Integer, Integer>> longestSequesForDiff = new ArrayList<>();
+        
+        for (int hiPtr = 0; hiPtr < nums.length; hiPtr++) {
+            
+            Map<Integer, Integer> idxDiffAndCount = new HashMap<>(); 
+            longestSequesForDiff.add(idxDiffAndCount);
+            
+            for (int lPtr = 0; lPtr < hiPtr; lPtr++) {
+                int diff = nums[lPtr] - nums[hiPtr];
+                int seqLen = longestSequesForDiff.get(lPtr).getOrDefault(diff, 1);
+                idxDiffAndCount.put(diff, seqLen+1);
+                maxSeqLen = Math.max(maxSeqLen, idxDiffAndCount.get(diff));
+            } 
+        }
+        
+        return maxSeqLen;
+    }
+}
