672a34bfe36f0e589142d7fb50e0404cad5116e3
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-06-28 23:16:36 +0530

    Time: 3 ms (71.55%), Space: 42.9 MB (80.47%)

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         
-        
         for (int i = 0; i < 9; i++) {
             
             Set<Character> valuesInRow = new HashSet<>();
@@ -16,7 +15,6 @@ class Solution {
                 int colStart = 3 * (i %3), colOffset = j % 3;
                 int rowStart = 3 * (i / 3), rowOffset = j / 3;
             
-                // System.out.println(3*(i/3));
                 char blockVal = board[rowOffset + rowStart][colOffset + colStart];
                 
                 if (rowVal != '.' && !valuesInRow.add(rowVal)) 
@@ -25,12 +23,9 @@ class Solution {
                     return false;
                 if (blockVal != '.' && !valuesInBlock.add(blockVal)) 
                     return false;
-            }
-            
+            }            
         }
         
         return true;
-        
-        
     }
 }
