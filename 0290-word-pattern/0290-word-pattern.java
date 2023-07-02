75e74aeae41c22c959767ec7687e928edef878b3
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-07-02 21:56:31 +0530

    Time: 1 ms (61.99%), Space: 40.2 MB (94.28%)

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -8,33 +8,24 @@ class Solution {
         if (words.length != pattern.length()) {
             return false;
         }
-        
-        
+         
         for (int i = 0; i < words.length; i++) {
             
             char ch = pattern.charAt(i);
             
             if (charWordMapping.containsKey(ch)) {
-                
                 if (!charWordMapping.get(ch).equals(words[i])) {
                     return false;
-                }
-                
+                }  
             }
             else {
-               
                 if (charWordMapping.containsValue(words[i])) {
                     return false;
                 }
-                
                charWordMapping.put(ch, words[i]); 
-            }
-            
-            
+            } 
         }
         
         return true;
-        
-        
     }
 }
