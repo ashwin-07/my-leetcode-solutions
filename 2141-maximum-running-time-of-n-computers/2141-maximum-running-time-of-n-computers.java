83bd75aed9303c084e580ad417ee5999e5e62124
Ashwin <67856771+ashwin-07@users.noreply.github.com>
2023-07-27 21:17:27 +0530

    Time: 20 ms (71.82%), Space: 60.9 MB (6.36%)

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long maxRunTime(int n, int[] batteries) {
+        
+        long totalPower = Arrays.stream(batteries)
+                                .mapToLong(num -> Long.valueOf(num))
+                                .sum();
+        long left = 1, right = totalPower / n;
+        
+        while (left < right) {
+            long currTarget = right - (right - left) / 2;
+            long requiredPowerForCurrTarget = 0;
+             for (int power:batteries) {
+                 requiredPowerForCurrTarget+=Math.min(power, currTarget);
+             }
+            if (requiredPowerForCurrTarget >= (long)(n * currTarget)) {
+                left = currTarget;
+            }
+            else {
+                right = currTarget - 1;
+            }
+            
+        }
+        
+        return left;
+        
+    }
+}
